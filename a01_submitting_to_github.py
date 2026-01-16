1a815109e06897277b5c68bd15dbde64b5c97da9
ehess29 <ehess29@byu.edu>
2026-01-16 11:49:32 -0700

    Update a01_submitting_to_github.py

--- a01_submitting_to_github.py
+++ a01_submitting_to_github.py
@@ -1,11 +1,7 @@
-# Name: <Enter your name>
-# Description: <Enter a short description of your code>
-'''
-Write your code below these comments.
+# Name: Elizabeth Hess
+# Description: This is inputting a number and dividing it by 2.
 
-You can test your code using the testing tab on the left.
-When finished, upload your code by committing it and pushing it back up to 
-GitHub. I recommend using GitHub Desktop to do that.
-
-You can delete these comments after reading them.
-'''
+num = input("Please enter a whole number: ")
+num_int = int(num) # this is converting the string number to an integer number
+new_number = num_int / 2
+print(f"{num_int} divided by 2 is {new_number}.") # print out the final result
